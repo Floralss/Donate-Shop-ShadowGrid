3b594b4bb9b909b998511c562c6447393c63be81
Floralss <strepoomich27@gmail.com>
2026-06-06 15:29:08 +0300

    Delete launcher.js

--- launcher.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("sendMessage").onclick = () => {
-const msg = document.getElementById("message").value;
-
-const div = document.createElement("div");
-div.innerHTML = "<p>"+msg+"</p>";
-
-document.getElementById("messages").appendChild(div);
-};
